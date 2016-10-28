b50257a3333ea94dd407c3ec8cb4bcfc054f8f3a
Shawn Cramp <cram7290@mylaurier.ca>
2016-10-28 11:25:04 -0400

    init commit

---
+++ src/apriori.py
@@ -0,0 +1,46 @@
+""" ---------------------------------------------------------
+Author: Shawn Cramp
+ID: 111007290
+Author: Omar Farah
+ID:
+Author: Nick Hare
+ID:
+Description: CP465 Final Assignment
+Date: October, 28th, 2016
+-------------------------------------------------------------
+Assignment Task:
+The objective of this project is to implement two Data Mining
+algorithms and test your implementations with various reasonably
+large datasets. The term reasonably large is to be interpreted
+as anywhere from a few hundreds to a few thousands instances (tuples).
+
+You need to implement two Data Mining algorithms seen in class:
+1. the ID3 algorithm, based on entropy/information gain, to build decision trees;
+2. the Apriori algorithm for mining association rules;
+
+-------------------------------------------------------------
+Import Declarations ------------------------------------- """
+
+
+""" ---------------------------------------------------------
+Global Declarations ------------------------------------- """
+
+
+""" ---------------------------------------------------------
+Class Declarations -------------------------------------- """
+
+
+""" ---------------------------------------------------------
+Function Declarations ----------------------------------- """
+
+
+""" ---------------------------------------------------------
+Console Execution Functions ---------------------------------
+All functions below are used for execution of the program and
+are no involved in the logical process.
+The Main function will be called upon code execution and the
+user will be presented with a list of options on how to
+proceed and view the code output he/she would like to view.
+-------------------------------------------------------------
+--------------------------------------------------------- """
+
---
+++ src/reader.py
@@ -0,0 +1,35 @@
+""" ---------------------------------------------------------
+Author: Shawn Cramp
+ID: 111007290
+Author: Omar Farah
+ID:
+Author: Nick Hare
+ID:
+Description: CP465 Final Assignment
+Date: October, 28th, 2016
+-------------------------------------------------------------
+Import Declarations ------------------------------------- """
+
+""" ---------------------------------------------------------
+Global Declarations ------------------------------------- """
+
+
+""" ---------------------------------------------------------
+Class Declarations -------------------------------------- """
+
+
+class DataSet:
+    """
+    DataSet class for containing the information from the imported
+    dataset
+    """
+    def __init__(self, filename):
+        self.filename = filename
+        self._values = []
+
+    def create(self):
+        """
+        Read Dataset File and store information into self._values
+        :return:
+        """
+        pass
